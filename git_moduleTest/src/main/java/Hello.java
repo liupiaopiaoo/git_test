5ed62e0461139671a5411b8ed75f18b8bdd5efc1
liupiaopiao <1185423565@qq.com>
2022-05-07 18:00:01 +0800

    第二次更新

--- git_moduleTest/src/main/java/Hello.java
+++ git_moduleTest/src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Hello Git! - update 1!");
+		System.out.println("Hello Git! - update 2!");
 	}
 }
