fa5c713e4f1b02228d144226ae6aff64fcb7bfbd
liupiaopiao <1185423565@qq.com>
2022-05-07 18:03:39 +0800

    创建分支

--- git_moduleTest/src/main/java/Hello.java
+++ git_moduleTest/src/main/java/Hello.java
@@ -2,7 +2,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Hello Git! - update 1!");
-		System.out.println("Hello Git! - update 2!");
-		System.out.println("Hello Git! - update 3!");
+		System.out.println("创建分支!");
 	}
 }
